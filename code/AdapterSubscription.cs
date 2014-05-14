bf36f5248121439beb149b38577ec41410109185
Fredrik Tonn <fredrik.tonn@xlent.se>
2014-05-14 08:36:58 +0200

    Created method to use built in Client.OnMessage to handle incomming requests in AdapterSubscription

--- code/AdapterSubscription.cs
+++ code/AdapterSubscription.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 using Microsoft.ServiceBus.Messaging;
 using Xlent.Match.ClientUtilities.Messages;
+using Xlent.Match.ClientUtilities.ServiceBus;
 
 namespace Xlent.Match.ClientUtilities
 {
-    public class AdapterSubscription : ServiceBus.Subscription
+    public class AdapterSubscription : Subscription
     {
-        private static ServiceBus.Topic _requestTopic;
-        private static ServiceBus.Topic _responseTopic;
+        private static readonly Topic RequestTopic;
+        private static readonly Topic ResponseTopic;
 
         static AdapterSubscription()
         {
-            _requestTopic = new ClientUtilities.ServiceBus.Topic("Xlent.Match.ClientUtilities.ConnectionString", "Request");
-            _responseTopic = new ServiceBus.Topic("Xlent.Match.ClientUtilities.ConnectionString", "Response");
+            // The maximum number of concurrent connections allowed by a ServicePoint object. The default value is 2.
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            RequestTopic = new Topic("Xlent.Match.ClientUtilities.ConnectionString", "Request");
+            ResponseTopic = new Topic("Xlent.Match.ClientUtilities.ConnectionString", "Response");
         }
 
         private AdapterSubscription(string name, Filter filter)
-            : base(_requestTopic, name, filter)
+            : base(RequestTopic, name, filter)
         {
         }
 
@@ -46,7 +52,21 @@ namespace Xlent.Match.ClientUtilities
             return new SqlFilter(string.Format("ClientName = '{0}' AND EntityName = '{1}'", clientName, entityName));
         }
 
-        public delegate Messages.SuccessResponse RequestDelegate(Request request);
+        public delegate SuccessResponse RequestDelegate(Request request);
+
+        public void ProcessRequests(RequestDelegate requestDelegate, ManualResetEvent stopEvent, int maxConcurrentCalls = 1 )
+        {
+            var options = new OnMessageOptions { AutoComplete = false, MaxConcurrentCalls = maxConcurrentCalls };
+
+            Client.OnMessage(message =>
+            {
+                var request = message.GetBody<Request>();
+
+                SafeProcessRequest(requestDelegate, request, message);
+            }, options);
+
+            stopEvent.WaitOne();
+        }
 
         public void HandleOne(RequestDelegate requestDelegate)
         {
@@ -54,6 +74,11 @@ namespace Xlent.Match.ClientUtilities
             var request = GetOneMessage<Request>(out message);
             if (request == null) return;
 
+            SafeProcessRequest(requestDelegate, request, message);
+        }
+
+        private static void SafeProcessRequest(RequestDelegate requestDelegate, Request request, BrokeredMessage message)
+        {
             try
             {
                 var successResponse = requestDelegate(request);
@@ -62,18 +87,18 @@ namespace Xlent.Match.ClientUtilities
             catch (Exceptions.MovedException exception)
             {
                 var oldId = request.KeyValue;
-                var failureResponse = new Messages.FailureResponse(request, exception.ErrorType)
+                var failureResponse = new FailureResponse(request, exception.ErrorType)
                 {
                     Value = oldId,
-                    Message = exception.Message
+                    Message = exception.Message,
+                    Key = {Value = exception.NewKeyValue}
                 };
-                failureResponse.Key.Value = exception.NewKeyValue;
 
                 SendResponse(failureResponse);
             }
             catch (Exceptions.MatchException exception)
             {
-                var failureResponse = new Messages.FailureResponse(request, exception.ErrorType)
+                var failureResponse = new FailureResponse(request, exception.ErrorType)
                 {
                     Message = exception.Message
                 };
@@ -85,13 +110,18 @@ namespace Xlent.Match.ClientUtilities
                 message.Abandon();
                 throw;
             }
-
             message.Complete();
+
+        }
+
+        public void Close()
+        {
+            Client.Close();
         }
 
         private static void SendResponse<T>(T response) where T : Response
         {
-            _responseTopic.Send<T>(response, new Dictionary<string, object>() { { "ResponseType", response.ResponseType } });
+            ResponseTopic.Send(response, new Dictionary<string, object> { { "ResponseType", response.ResponseType } });
         }
     }
 }
