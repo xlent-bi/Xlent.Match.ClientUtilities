17199fc7fbe81c0869b9f614d4bf846de45157aa
vanbrayne <lars.lindberg@xlent.se>
2014-05-19 15:24:42 +0200

    Bugs calling String.Format()

--- code/Logging/Log.cs
+++ code/Logging/Log.cs
@@ -32,46 +32,51 @@ namespace Xlent.Match.ClientUtilities.Logging
 
         public static void Critical(string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Critical, message, null);
+            LogEvent(TraceEventType.Critical, null, message, parameters);
         }
 
         public static void Critical(Exception exception, string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Critical, message, exception);
+            LogEvent(TraceEventType.Critical, exception, message, parameters);
         }
 
         public static void Error(string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Error, message, null);
+            LogEvent(TraceEventType.Error, null, message, parameters);
         }
 
         public static void Error(Exception exception, string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Error, message, exception);
+            LogEvent(TraceEventType.Error, exception, message, parameters);
         }
 
         public static void Warning(string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Warning, message, null);
+            LogEvent(TraceEventType.Warning, null, message, parameters);
         }
 
         public static void Warning(Exception exception, string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Warning, message, exception);
+            LogEvent(TraceEventType.Warning, exception, message, parameters);
         }
 
         public static void Information(string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Information, String.Format(message, parameters));
+            LogEvent(TraceEventType.Information, null, message, parameters);
         }
 
         public static void Verbose(string message, params object[] parameters)
         {
-            LogEvent(TraceEventType.Verbose, message);
+            LogEvent(TraceEventType.Verbose, null, message, parameters);
         }
 
-        private static void LogEvent(TraceEventType eventType, string message, Exception exception = null)
+        private static void LogEvent(TraceEventType eventType, Exception exception, string message, params object[] parameters)
         {
+            if (parameters.Length > 0)
+            {
+                message = String.Format(message, parameters);
+            }
+
             var logMessage = String.Format("{0}{1}{2}.", message, (exception == null) ? "" : ": ",
                 (exception == null) ? "" : ExtractExceptionMessages(exception));
 
